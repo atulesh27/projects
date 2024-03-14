027aad75f978b17126198c782626cacc12421329
atulesh27 <133364113+atulesh27@users.noreply.github.com>
2024-03-14 16:39:11 +0530

    Delete scooden directory

--- scooden/bookdemo/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import BookDemoModel
-
-
-class BookDemoForm(forms.ModelForm):
-    class Meta:
-        model = BookDemoModel
-        fields = '__all__'
--- scooden/courseinfo/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import StudRegistration
-
-
-class StudRegistrationForm(forms.ModelForm):
-    class Meta:
-        model = StudRegistration
-        fields = '__all__'
